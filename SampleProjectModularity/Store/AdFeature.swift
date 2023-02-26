b3d6058aa3c312ed87378b21f192aef39550cd64
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2023-02-26 23:00:11 +0100

    clean up a little bit directories

--- SampleProjectModularity/Store/AdFeature.swift
+++ SampleProjectModularity/Store/AdFeature.swift
@@ -1,19 +1,19 @@
 import ComposableArchitecture
 
 struct AdFeature: ReducerProtocol {
-    func reduce(into state: inout State, action: Action) -> ComposableArchitecture.EffectTask<Action> {
-        switch action {
-        case let .searchParameterUpdated(newSearchParameters):
-            state.searchParameters = newSearchParameters
-            return .none
-        }
-    }
-    
     struct State: Equatable {
         var searchParameters: SearchParametersModel
     }
     
     enum Action: Equatable {
         case searchParameterUpdated(SearchParametersModel)
+    }
+    
+    func reduce(into state: inout State, action: Action) -> ComposableArchitecture.EffectTask<Action> {
+        switch action {
+        case let .searchParameterUpdated(newSearchParameters):
+            state.searchParameters = newSearchParameters
+            return .none
+        }
     }
 }
