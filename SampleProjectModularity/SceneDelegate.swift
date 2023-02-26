300d1f5917797ff4387bf343bd1376a32347c127
Amir Hossein Baradaran Toutounchi <amirhossein.baradarantutunchi@adevinta.com>
2023-02-26 20:32:18 +0100

    init AdViewController with Store

--- SampleProjectModularity/SceneDelegate.swift
+++ SampleProjectModularity/SceneDelegate.swift
@@ -1,10 +1,19 @@
 import UIKit
+import ComposableArchitecture
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        configureWindow()
+    }
+    private func configureWindow() {
+        let store = Store(initialState: AdFeature.State(searchParameters: SearchParametersModel()), reducer: AdFeature())
+        let adController = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(identifier: "AdViewController") { coder in
+            AdViewController(coder: coder, viewStore: ViewStore(store))
+        }
     }
 }
 
